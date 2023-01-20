1e87a7355884b0f70da2402af93e781b192b5918
Ubuntu <ubuntu@ip-172-31-0-137.ec2.internal>
2023-01-20 23:46:11 +0000

    Coment

--- src/main/java/br/ufs/dcomp/InetAddressTest/App.java
+++ src/main/java/br/ufs/dcomp/InetAddressTest/App.java
@@ -2,20 +2,18 @@ package br.ufs.dcomp.InetAddressTest;
 
 import java.net.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         try{
             
-            String name = "www.ufs.br";
+            String name = "www.google.com";
             InetAddress address = InetAddress.getByName(name); 
             System.out.println( "Name:      "+ name);
+            for (int=0; int<address.lenght; int++){
             System.out.println( "Address:   "+ address.getHostAddress());
+            }
         } catch (UnknownHostException e){
             System.out.println(e.getMessage());
         }                
